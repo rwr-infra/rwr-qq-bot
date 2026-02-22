3964e26e6ffddbb4501e6b40ad0415405895047a
Kreedzt <zhaozisong1@live.com>
2026-02-22 14:40:04 +0800

    :bug: fixed welcome group listen

--- src/notices/welcome.ts
+++ src/notices/welcome.ts
@@ -1,13 +1,26 @@
 import * as fs from 'fs';
-import { NoticeExecCtx } from "../types";
+import { NoticeExecCtx } from '../types';
 
 let template = '';
 
+const shouldHandleWelcome = (ctx: NoticeExecCtx): boolean => {
+    const listenGroup = Number(ctx.env.LISTEN_GROUP);
+    if (!Number.isNaN(listenGroup) && ctx.event.group_id !== listenGroup) {
+        return false;
+    }
+
+    return true;
+};
+
 export const welcomeNewMember = async (ctx: NoticeExecCtx) => {
+    if (!shouldHandleWelcome(ctx)) {
+        return;
+    }
+
     if (!template) {
         const templateFileName = ctx.env.WELCOME_TEMPLATE;
         const content = fs.readFileSync(templateFileName, 'utf-8');
         template = content;
     }
     await ctx.reply(template);
-}
+};
